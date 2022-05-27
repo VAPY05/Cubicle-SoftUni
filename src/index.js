65efe8bcffca518897f422e7588817ed623f074e
VAPY05 <74071882+VAPY05@users.noreply.github.com>
2022-05-27 18:19:09 +0300

    load resources

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const handlebars = require("express-handlebars")
 const app = express();
 const port = 5000;
 
-app.use(express.static("public"))
+app.use("/static",express.static("src/public"))
 
 app.engine("hbs",handlebars.engine({
     extname: "hbs"
@@ -13,9 +13,6 @@ app.engine("hbs",handlebars.engine({
 app.set("view engine", "hbs")
 app.set('views','./src/views')
 
-app.get("/",(req,res)=>{
-    res.render("index")
-});
 
 app.get("/",(req,res)=>{
     res.render("index")
