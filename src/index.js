dff0ad233fcd1924751858fef4487f5e53067d5f
VAPY05 <74071882+VAPY05@users.noreply.github.com>
2022-05-27 18:07:22 +0300

    load resources

--- src/index.js
+++ src/index.js
@@ -1,9 +1,39 @@
 const express = require("express");
+const handlebars = require("express-handlebars")
 
 const app = express();
 const port = 5000;
 
+app.use(express.static("public"))
+
+app.engine("hbs",handlebars.engine({
+    extname: "hbs"
+}))
+
+app.set("view engine", "hbs")
+app.set('views','./src/views')
+
 app.get("/",(req,res)=>{
-    res.send("Home")
+    res.render("index")
+});
+
+app.get("/",(req,res)=>{
+    res.render("index")
+});
+
+app.get("/about",(req,res)=>{
+    res.render("about")
+});
+
+app.get("/create",(req,res)=>{
+    res.render("create")
+});
+
+app.get("/details/:id",(req,res)=>{
+    res.render("index")
+});
+
+app.get("*",(req,res)=>{
+    res.render("404")
 });
 app.listen(port,()=>{console.log(`Server is listening on port ${port}`)})
